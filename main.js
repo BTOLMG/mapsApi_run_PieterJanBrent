5e7286d9004a9c23dff560db1a386a999540e8f3
brent <brentthierens2006@gmail.com>
2026-03-27 12:25:25 +0100

    V0.5 - refresh location

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-// navigator.geolocation is ingebouwd in de browser, geen extra library nodig
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        function (positie) {
-            const lat = positie.coords.latitude;
-            const lng = positie.coords.longitude;
-
-            // Centreer de kaart op de gebruiker
-            kaart.setView([lat, lng], 16);
-        },
-        function (fout) {
-            // Wordt opgeroepen als de gebruiker toestemming weigert of er een fout optreedt
-            alert('Locatie kon niet worden bepaald: ' + fout.message);
-        }
-    );
-} else {
-    alert('Geolocatie wordt niet ondersteund door deze browser.');
-}
-
 function addPoint(){
     if (localStorage.points) {
         localStorage.points = Number(localStorage.points) + 1;
     } else {
         localStorage.points = 1;
     } 
+}
+
+function getLocation(){
+    console.log("HI")
+    // navigator.geolocation is ingebouwd in de browser, geen extra library nodig
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function (positie) {
+                const lat = positie.coords.latitude;
+                const lng = positie.coords.longitude;
+
+                L.marker([lat, lng])
+                    
+                    .addTo(kaart);
+            },
+            function (fout) {
+                // Wordt opgeroepen als de gebruiker toestemming weigert of er een fout optreedt
+                alert('Locatie kon niet worden bepaald: ' + fout.message);
+            }
+        );
+    } else {
+        alert('Geolocatie wordt niet ondersteund door deze browser.');
+    }
 }
