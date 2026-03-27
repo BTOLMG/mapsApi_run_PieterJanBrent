b630f090434b469412039751d33abb36297ef529
brent <brentthierens2006@gmail.com>
2026-03-27 11:59:39 +0100

    V0.2

---
+++ main.js
@@ -0,0 +1,33 @@
+// navigator.geolocation is ingebouwd in de browser, geen extra library nodig
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function (positie) {
+            const lat = positie.coords.latitude;
+            const lng = positie.coords.longitude;
+            const nauwkeurigheid = positie.coords.accuracy; // in meters
+
+            // Centreer de kaart op de gebruiker
+            kaart.setView([lat, lng], 16);
+
+            // Marker op de gebruikerslocatie
+            L.marker([lat, lng])
+                .addTo(kaart)
+                .bindPopup('📍 Jij bevindt je hier!<br>Nauwkeurigheid: ~' + Math.round(nauwkeurigheid) + ' m')
+                .openPopup();
+
+            // Optioneel: cirkel om nauwkeurigheid te visualiseren
+            L.circle([lat, lng], {
+                radius: nauwkeurigheid,
+                color: '#3388ff',
+                fillColor: '#3388ff',
+                fillOpacity: 0.1
+            }).addTo(kaart);
+        },
+        function (fout) {
+            // Wordt opgeroepen als de gebruiker toestemming weigert of er een fout optreedt
+            alert('Locatie kon niet worden bepaald: ' + fout.message);
+        }
+    );
+} else {
+    alert('Geolocatie wordt niet ondersteund door deze browser.');
+}
