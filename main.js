8a9680c8e6ea2400825da833bf16b2bb1b07027b
brent <brentthierens2006@gmail.com>
2026-03-27 12:09:27 +0100

    V0.3 - border

--- main.js
+++ main.js
@@ -4,24 +4,9 @@ if (navigator.geolocation) {
         function (positie) {
             const lat = positie.coords.latitude;
             const lng = positie.coords.longitude;
-            const nauwkeurigheid = positie.coords.accuracy; // in meters
 
             // Centreer de kaart op de gebruiker
             kaart.setView([lat, lng], 16);
-
-            // Marker op de gebruikerslocatie
-            L.marker([lat, lng])
-                .addTo(kaart)
-                .bindPopup('📍 Jij bevindt je hier!<br>Nauwkeurigheid: ~' + Math.round(nauwkeurigheid) + ' m')
-                .openPopup();
-
-            // Optioneel: cirkel om nauwkeurigheid te visualiseren
-            L.circle([lat, lng], {
-                radius: nauwkeurigheid,
-                color: '#3388ff',
-                fillColor: '#3388ff',
-                fillOpacity: 0.1
-            }).addTo(kaart);
         },
         function (fout) {
             // Wordt opgeroepen als de gebruiker toestemming weigert of er een fout optreedt
